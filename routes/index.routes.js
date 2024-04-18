33dee3ffb94951957463e08c772f81cf9238be67
Alexis <alexismoreauavila@gmail.com>
2024-04-18 15:59:57 +0200

    m

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -10,6 +10,6 @@ router.use("/events", require("./event.routes"));
 
 router.use("/comments", require("./comment.routes"));
 
-router.use("/joigning", require("./Joigners.routes"));
+router.use("/joigning", require("./joigners.routes"));
 
 module.exports = router;
